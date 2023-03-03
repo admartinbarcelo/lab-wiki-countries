7ff15f630554902a40d3f5c6daf8b009120b8377
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-03-03 10:18:46 +0100

    axios get 4 iteracion

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,36 @@
-import countriesJSON from './countries.json'
-import  NavBar from './components/NavBar';
-import  CountriesList  from './components/CountriesList';
-import  CountryDetails  from './components/CountryDetails';
-import { useState } from 'react';
+import countriesJSON from './countries.json';
+import NavBar from './components/NavBar';
+import CountriesList from './components/CountriesList';
+import CountryDetails from './components/CountryDetails';
+import { useState, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  const [countries, setCountries] = useState(countriesJSON)
+  const [countries, setCountries] = useState(countriesJSON);
+
+  useEffect(() => {
+    axios
+      .get('https://ih-countries-api.herokuapp.com/countries')
+      .then(response => {
+        setCountries(response.data)
+      })
+      .catch(err => {
+        console.error(err)
+      });
+  },);
 
   return (
     <div className="App">
-    <NavBar />
-    <CountriesList countries ={countries} />
-    <Routes>
-        {/* <Route path="/" element={<CountriesList countries ={countries} />} /> */}
-        <Route path=":id" element={<CountryDetails countries={countries}/>} />
+      <NavBar />
+      <CountriesList countries={countries} />
+      <Routes>
+        <Route path="/:id" element={<CountryDetails countries={countries} />} />
       </Routes>
     </div>
-  )
+  );
 }
 
 export default App;
+
